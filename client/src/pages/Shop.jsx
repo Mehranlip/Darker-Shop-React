fdb78828864f83998c2ee38861cd39e557da8d0d
Mehran <contact@mehranlip.ir>
2023-08-13 15:39:18 +0330

    add range price filtered

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -8,7 +8,10 @@ import productCat from "../data/categoeis";
 import ProductItem from './../components/ProductItem';
 
 
-const getFilteredItem = (query, items, checkBoxValue) => {
+const getFilteredItem = (query, items, checkBoxValue, rangePrice) => {
+    if (rangePrice) {
+        return items.filter((item) => item.price >= rangePrice)
+    }
     if (checkBoxValue) {
         return items.filter((item) => item.cat.includes(checkBoxValue))
     }
@@ -22,10 +25,10 @@ const getFilteredItem = (query, items, checkBoxValue) => {
 function Shop() {
     const [query, setQuery] = useState("")
     const [checkBoxValue, setCheckboxValue] = useState()
+    const [rangePrice, setRangePrice] = useState(0)
 
-    const filteredItems = getFilteredItem(query, productList, checkBoxValue)
+    const filteredItems = getFilteredItem(query, productList, checkBoxValue, rangePrice)
 
-    console.log(checkBoxValue);
 
     const handleCheckBoxValue = (e) => {
         const newData = e.target.value;
@@ -34,26 +37,52 @@ function Shop() {
 
     return (
         <div dir="rtl">
-            <div>
-                <input className="mt-3 rounded p-1" type="text" placeholder="جستجو محصول" dir="rtl" onChange={(e) => setQuery(e.target.value)} />
-                {productCat.map((item) => (
-                    <label className="mx-2" key={item.cat}>
-                        <input className="mx-1"
-                            onChange={handleCheckBoxValue}
-                            type="checkbox"
-                            value={item.cat} />
-                        {item.namecat}
-                    </label>
-                ))}
+            <Row>
+                <Col sm={12} md={2}>
+                    <div className="row">
+                        {/* input search */}
+                        <input className="mt-2 rounded p-1" type="text" placeholder="جستجو محصول" dir="rtl" onChange={(e) => setQuery(e.target.value)} />
+                        {/* end input search */}
+                        {/* start filtered cat */}
+                        <hr className="m-2" />
+                        <span>فیلتر بر اساس دسته بندی</span>
+                        <hr className="m-2" />
+                        {productCat.map((item) => (
+                            <ul className="list-unstyled">
+                                <li >
+                                    <label key={item.cat}>
+                                        <input className="mx-2"
+                                            onChange={handleCheckBoxValue}
+                                            type="checkbox"
+                                            value={item.cat} />
+                                        {item.namecat}
+                                    </label>
+                                </li>
+                            </ul>
+                        ))}
+                        {/* end filtered cat */}
+                        {/* start filtered price */}
+                        <hr className="m-2" />
+                        <span>فیلتر بر اساس قیمت</span>
+                        <hr className="m-2" />
+                        <label for="customRange2" class="form-label">{`از 0 تا ${rangePrice}`} {" "}تومان</label>
+                        <input type="range" class="form-range" min="0" max="180000000" id="customRange2" onChange={(e) => setRangePrice(e.target.value)}></input>
+                        {/* end filtered price  */}
 
-            </div>
-            <Row xs={1} md={4} className="g-4">
-                {filteredItems.map((item) => (
-                    <Col key={item.id} align="center">
-                        <ProductItem product={item} />
-                    </Col>
-                ))}
+                    </div>
+                </Col>
+                <Col sm={12} md={10}>
+                    <Row xs={1} md={3} className="g-3 p-4">
+                        {filteredItems.map((item) => (
+                            <Col key={item.id} align="center">
+                                <ProductItem product={item} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Col>
             </Row>
+
+
         </div>
     )
 }
