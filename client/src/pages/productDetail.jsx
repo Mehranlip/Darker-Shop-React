dc889309923fa383039154d9e956e55bafb56188
Mehran <contact@mehranlip.ir>
2023-08-14 17:43:58 +0330

    end detail page

--- client/src/pages/productDetail.jsx
+++ client/src/pages/productDetail.jsx
@@ -1,15 +1,68 @@
 import React from "react"
 import { useParams } from "react-router-dom"
 import { productList } from "../data/items"
+import { Row, Col, Button, Form } from "react-bootstrap"
+
+import { useContext } from "react"
+import { cartContext } from "../context/CartContext"
 
 function ProductDetail() {
+
     const { productId } = useParams()
     const thisProduct = productList.find(prod => prod.id === productId)
 
+
+    const cart = useContext(cartContext)
+    const productQuantity = cart.getProductQuantity(thisProduct.id)
+
+
+    console.log(productQuantity);
+
     return (
-        <div>
-            <h1>{thisProduct.title}</h1>
-        </div>
+        <Row className="mt-5">
+            <Col className="order-1 order-md-0" sm={12} md={8}>
+                <div className="p-0 pm-5">
+                    <h1>{thisProduct.title}</h1>
+                    <p className="text-justify">
+                        {thisProduct.description}
+                    </p>
+                    {/* add to cart button */}
+                    <Row>
+                        <Col sm={12} md={5}>
+                            <div className="mt-3">
+                                {productQuantity > 0 ? (
+                                    <>
+                                        <Form as={Row}>
+                                            <Form.Label column="true" sm={6} className="text-white">
+                                                تعداد : {productQuantity}
+                                            </Form.Label>
+                                            <Col sm="6">
+                                                <Button sm="6" className="mx-2 text-white" variant="btn btn-outline-secondary"
+                                                    onClick={() => cart.addItemToCart(thisProduct.id)}
+                                                >+</Button>
+                                                <Button sm="6" className="mx-2 text-white" variant="btn btn-outline-secondary"
+                                                    onClick={() => cart.removeItemFromCart(thisProduct.id)}
+                                                >-</Button>
+                                            </Col>
+                                        </Form>
+                                    </>
+                                ) : (
+                                    <Button onClick={() => cart.addItemToCart(thisProduct.id)} variant="btn btn-outline-secondary" className="text-white">
+                                        افزودن به سبد خرید
+                                    </Button>
+                                )}
+                            </div>
+                        </Col>
+                    </Row>
+                    {/* end add to button cart */}
+                </div>
+            </Col>
+            <Col className="order-0 order-md-1" sm={12} md={4}>
+                <div className="p-3">
+                    <img className="rounded" src={thisProduct.image} width="320" height="300" />
+                </div>
+            </Col>
+        </Row>
     )
 }
 
